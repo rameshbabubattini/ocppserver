456bf4bc66d163f62286accab4f9302a1450c241
Ramesh Babu Battini <rameshbabu.battini@gmail.com>
2018-03-25 07:19:39 +0530

    OCPP Server

---
+++ src/main/java/com/ocpp/OcppServerApplication.java
@@ -0,0 +1,12 @@
+package com.ocpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcppServerApplication.class, args);
+	}
+}
